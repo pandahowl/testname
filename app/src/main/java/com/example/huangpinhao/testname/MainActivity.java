246ef2c71599bfcc9e95814b4e3b6b0cb1062fee
pandahowl <pandahowl@gmail.com>
2015-07-01 12:31:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/huangpinhao/testname/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.huangpinhao.testname;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private EditText text;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (EditText) findViewById(R.id.edittext01);
+        button =(Button) findViewById(R.id.button1);
+        button.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                RadioButton celsiusButton = (RadioButton) findViewById(R.id.horizontal);
+                RadioButton fahrenheitButton = (RadioButton) findViewById(R.id.vertical);
+                if (text.getText().length() == 0) {
+                    Toast.makeText(this, "Please enter a valid number",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                float inputValue = Float.parseFloat(text.getText().toString());
+                if (celsiusButton.isChecked()) {
+                    text.setText(String
+                            .valueOf(ConverterUtil.convertFahrenheitToCelsius(inputValue)));
+                    celsiusButton.setChecked(false);
+                    fahrenheitButton.setChecked(true);
+                } else {
+                    text.setText(String
+                            .valueOf(ConverterUtil.convertCelsiusToFahrenheit(inputValue)));
+                    fahrenheitButton.setChecked(false);
+                    celsiusButton.setChecked(true);
+                }
+                break;
+        }
+    }
+
+        @Override
+        public boolean onCreateOptionsMenu (Menu menu){
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected (MenuItem item){
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+    }
---
+++ app/src/main/java/com/example/huangpinhao/testname/ScrollViewActivity.java
@@ -0,0 +1,22 @@
+package com.example.huangpinhao.testname;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+/**
+ * Created by huangpinhao on 2015/7/1.
+ */
+public class ScrollViewActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.testview);
+        TextView view =  (TextView) findViewById(R.id.TextView02);
+        String s="";
+        for (int i=0; i < 500; i++) {
+            s += "vogella.com ";
+        }
+        view.setText(s);
+    }
+}
